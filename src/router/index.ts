b2f0a69b12c0e4c96c839b127207ce655a1052ca
tgenz1213 <timothy.genz@snhu.edu>
2025-08-14 23:00:22 -0500

    refactor: Remove unused routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,19 +7,6 @@ const routes: Array<RouteRecordRaw> = [
     name: 'home',
     component: HomeView,
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../components/sections/AboutSection.vue'),
-  },
-  {
-    path: '/projects',
-    name: 'projects',
-    component: () => import('../components/sections/ProjectsSection.vue'),
-  },
 ]
 const router: Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
