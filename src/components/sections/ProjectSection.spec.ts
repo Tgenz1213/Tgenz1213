193aaab1d9be0e302eaea3f612d1af8d3f4c60b6
tgenz1213 <timothy.genz@snhu.edu>
2025-09-01 14:07:19 -0500

    refactor: Move project data to separate file

--- src/components/sections/ProjectSection.spec.ts
+++ src/components/sections/ProjectSection.spec.ts
@@ -2,38 +2,9 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import ProjectsSection from './ProjectsSection.vue'
 import ProjectCard from '../ProjectCard.vue'
-import type { Project } from '../../types'
+import projects from '@/data/projects'
 
 describe('ProjectsSection', () => {
-  const projects: Project[] = [
-    {
-      id: 1,
-      title: 'Portfolio Website',
-      description:
-        'A responsive personal portfolio built with Vue.js and TypeScript, showcasing my skills and projects.',
-      image: 'vue_project_screen.png',
-      link: 'https://github.com/Tgenz1213/Tgenz1213',
-      tags: ['Vue.js', 'TypeScript', 'CSS'],
-    },
-    {
-      id: 2,
-      title: 'Weather App',
-      description:
-        'A React front end with Go backend that fetches weather data from the weather.gov API and caches the data with Redis.',
-      image: 'weather_screen.png',
-      link: 'https://github.com/Tgenz1213/weather',
-      tags: ['Node.js', 'React', 'Go', 'API'],
-    },
-    {
-      id: 3,
-      title: 'ML Maze Runner',
-      description: 'An ML model that solves the Treasure Hunt game.',
-      image: 'Treasure_Hunt_Game_map.png',
-      link: 'https://github.com/Tgenz1213/maze-rl-agent/blob/main/Week_7/TreasureHuntGame/Genz_Timothy_ProjectTwo.ipynb',
-      tags: ['Python', 'AI', 'ML'],
-    },
-  ]
-
   const wrapper = mount(ProjectsSection, {
     global: {
       stubs: {
