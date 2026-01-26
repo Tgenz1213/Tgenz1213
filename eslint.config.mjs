1a5d0de95406ddb98ffb50d48229f8fb032e2a91
Timothy Genz <34835862+Tgenz1213@users.noreply.github.com>
2026-01-25 18:27:15 -0600

    feat: integrate eslint-config-prettier for improved code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,13 @@ import pluginVue from 'eslint-plugin-vue'
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  eslintConfigPrettier,
   {
     languageOptions: {
       parser: pluginVue.parser,
